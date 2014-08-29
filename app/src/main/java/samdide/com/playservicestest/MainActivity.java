6141facb1739d6d63cd775f15cf885a755040d1b
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-08-29 20:40:38 +0200

    Initial commit

---
+++ app/src/main/java/samdide/com/playservicestest/MainActivity.java
@@ -0,0 +1,12 @@
+package samdide.com.playservicestest;
+
+/**
+ * Main activity for a simple game to test Google Play Game Services.
+ *
+ * The game is based on the TypeANumber example code provided by Google.
+ *
+ * Created by Henrik Samuelsson on 2014-08-24.
+ */
+public class MainActivity extends BaseGameActivity {
+
+}
